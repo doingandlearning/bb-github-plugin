a847d86c0c45dfc68bfe2d57fe2fb2f9f50108c7
Kevin Cunningham <kevin@kevincunningham.co.uk>
2022-09-28 09:41:31 +0100

    Initial commit

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,62 +1,65 @@
-import { IntegrationBase } from "@budibase/types"
-import fetch from "node-fetch"
+import { IntegrationBase } from "@budibase/types";
+import fetch from "node-fetch";
 
 interface Query {
-  method: string
-  body?: string
-  headers?: { [key: string]: string }
+  method: string;
+  body?: string;
+  headers?: { [key: string]: string };
 }
 
 interface JsonDict {
-  [key: string]: string | number | boolean | (string|number|boolean)[]
+  [key: string]: string | number | boolean | (string | number | boolean)[];
 }
 
 class CustomIntegration implements IntegrationBase {
-  private readonly url: string
-  private readonly pat: string
-  private readonly github_user: string
+  private readonly url: string;
+  private readonly pat: string;
+  private readonly github_user: string;
 
-  constructor(config: { repo_owner: string; pat: string; repo_name: string; user: string;}) {
+  constructor(config: {
+    repo_owner: string;
+    pat: string;
+    repo_name: string;
+    user: string;
+  }) {
     // https://api.github.com/repos/doingandlearning/budibase-issues/issues/1
-    this.url = `https://api.github.com/repos/${config.repo_owner}/${config.repo_name}/issues`
-    this.pat = config.pat
-    this.github_user = config.user
+    this.url = `https://api.github.com/repos/${config.repo_owner}/${config.repo_name}/issues`;
+    this.pat = config.pat;
+    this.github_user = config.user;
   }
 
   async request(url: string, opts: Query) {
     if (!this.pat) {
-      throw new Error("Need to provide a personal access token.")
+      throw new Error("Need to provide a personal access token.");
     }
-    
-    const auth = { authorization: `Bearer ${this.pat}` }
-    opts.headers = opts.headers ? { ...opts.headers, ...auth } : auth
-    
-    const response = await fetch(url, opts)
+
+    const auth = { authorization: `Bearer ${this.pat}` };
+    opts.headers = opts.headers ? { ...opts.headers, ...auth } : auth;
+
+    const response = await fetch(url, opts);
     if (response.status <= 300) {
       try {
-        const contentType = response.headers.get("content-type")
+        const contentType = response.headers.get("content-type");
         if (contentType?.includes("json")) {
-          return await response.json()
+          return await response.json();
         } else {
-          return await response.text()
+          return await response.text();
         }
       } catch (err) {
-        return await response.text()
+        return await response.text();
       }
     } else {
-      const err = await response.text()
-      throw new Error(err)
+      const err = await response.text();
+      throw new Error(err);
     }
   }
 
-  async create(query: { title: string; body: string; }) {
+  async create(query: { title: string; body: string }) {
     const body = {
-      "title": query.title,
-      "body": query.body,
-      "assignees": [
-        this.github_user
-      ]
-    }
+      title: query.title,
+      body: query.body,
+      assignees: [this.github_user],
+    };
 
     const opts = {
       method: "POST",
@@ -64,51 +67,50 @@ class CustomIntegration implements IntegrationBase {
       headers: {
         "Content-Type": "application/json",
       },
-    }
-    return this.request(this.url, opts)
+    };
+    return this.request(this.url, opts);
   }
 
   async read(query: { id: string }) {
     const opts = {
       method: "GET",
+    };
+    if (query.id) {
+      return this.request(`${this.url}/${query.id}`, opts);
     }
-    if(query.id) {
-      return this.request(`${this.url}/${query.id}`, opts)
-    }
-    return this.request(`${this.url}`, opts)
+    return this.request(`${this.url}`, opts);
   }
 
-
   async update(query: { json: JsonDict }) {
-    if(!query.json.id) {
-      throw new Error("You need to provide an issue id")
+    if (!query.json.id) {
+      throw new Error("You need to provide an issue id");
     }
-   const {id, ...body } = query.json
+    const { id, ...body } = query.json;
     const opts = {
-      method: "PUT",
+      method: "PATCH",
       body: JSON.stringify(body),
       headers: {
         "Content-Type": "application/json",
       },
-    }
-    return this.request(`${this.url}/${id}`, opts)
+    };
+    return this.request(`${this.url}/${id}`, opts);
   }
 
   async delete(query: { id: string }) {
     const opts = {
       method: "PATCH",
-      body: JSON.stringify({"state": "closed"})
-    }
-    return this.request(`${this.url}/${query.id}`, opts)
+      body: JSON.stringify({ state: "closed" }),
+    };
+    return this.request(`${this.url}/${query.id}`, opts);
   }
 
   async close(query: { id: string }) {
     const opts = {
       method: "PATCH",
-      body: JSON.stringify({"state": "closed"})
-    }
-    return this.request(`${this.url}/${query.id}`, opts)
+      body: JSON.stringify({ state: "closed" }),
+    };
+    return this.request(`${this.url}/${query.id}`, opts);
   }
 }
 
-export default CustomIntegration
+export default CustomIntegration;
